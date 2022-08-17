34ab2c3e096fc3bc1ebef217063b78f3554d8a42
Raphaël Vinot <raphael@vinot.info>
2022-08-17 21:39:29 +0200

    fix: Expose devices

--- playwrightcapture/__init__.py
+++ playwrightcapture/__init__.py
@@ -1,4 +1,5 @@
 from .capture import Capture  # noqa
+from .helpers import get_devices  # noqa
 from .exceptions import (PlaywrightCaptureException, UnknownPlaywrightDeviceType,  # noqa
                          UnknownPlaywrightBrowser, UnknownPlaywrightDevice,
                          InvalidPlaywrightParameter)
