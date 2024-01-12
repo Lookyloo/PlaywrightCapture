c8f20dca17716c5962d9e990e41f06582f413e11
Raphaël Vinot <raphael@vinot.info>
2024-01-12 10:51:00 +0100

    chg: Expose content with __all__

--- playwrightcapture/__init__.py
+++ playwrightcapture/__init__.py
@@ -3,3 +3,13 @@ from .helpers import get_devices  # noqa
 from .exceptions import (PlaywrightCaptureException, UnknownPlaywrightDeviceType,  # noqa
                          UnknownPlaywrightBrowser, UnknownPlaywrightDevice,
                          InvalidPlaywrightParameter)
+
+__all__ = [
+    'Capture',
+    'get_devices',
+    'PlaywrightCaptureException',
+    'UnknownPlaywrightDeviceType',
+    'UnknownPlaywrightBrowser',
+    'UnknownPlaywrightDevice',
+    'InvalidPlaywrightParameter'
+]
